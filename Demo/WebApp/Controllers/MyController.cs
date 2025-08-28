265dd230a51df183f95d1377f994bcd2b94df003
siphuc05 <nguyensiphuc26052005@gmail.com>
2025-08-28 18:11:57 +0700

    first commit

---
+++ Demo/WebApp/Controllers/MyController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class MyController : Controller
+    {
+        public IActionResult Index1()
+        {
+            return View();
+        }
+        public IActionResult MyInfor()
+        {
+            return View();
+        }
+    }
+}
---
+++ Demo/WebApp/Controllers/NewController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class NewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Sample()
+        {
+            return View();
+        }
+    }
+}
